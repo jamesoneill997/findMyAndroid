6dc10edb81713ee315b0972d9ab34ed8cc297ccf
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-25 19:01:48 +0100

    error handling and environment variable support

--- finder.py
+++ finder.py
@@ -14,27 +14,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from login import enter_email, enter_password
 from ring import ring
 import time
+import os
 
-
+#setup driver and timeout variables
+driver = webdriver.chrome.webdriver.WebDriver(executable_path='./chromedriver')
 timeout = 5
 
+try:
+    email = os.environ['pyemail']
+    password = os.environ['pypassword']
 
-driver = webdriver.chrome.webdriver.WebDriver(executable_path='./chromedriver')
+except KeyError:
+    print("You have not set up your credentials as environment variables. Please enter them now.")
+    email = os.environ['pyemail'] = str(input("Please enter your Google email address: "))
+    password = os.environ['pypassword'] = str(input("Please enter your Google password: "))
 
-
+#open webpage
 driver.get("https://www.google.com/android/find")
 
+#credentials
 enter_email(driver, email)
-
 WebDriverWait(driver, timeout).until( EC.presence_of_element_located((By.ID, "passwordNext")))
-
-try:
-    enter_password(driver, password)
-
-except ElementNotInteractableException:
-    time.sleep(2)
-    enter_password(driver, email)
-
+enter_password(driver, password)
 WebDriverWait(driver, timeout).until( EC.presence_of_element_located((By.ID, "device-infos")))
 
 try:
